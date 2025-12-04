028f08f63127e80ea4089dc6a9816e42c11e7662
jon-terschan <j.terschanski@posteo.net>
2025-12-04 13:51:56 +0200

    cds_api implementation

---
+++ CERRA/cds_api.py
@@ -0,0 +1,131 @@
+# BEAUTIFUL CDS IMPORT SCRIPT, IT NOW WORKS YEE
+# REFERENCE: https://confluence.ecmwf.int/display/CKB/How+to+install+and+use+CDS+API+on+Windows
+# import api, only works if the corresponding file with the key has been set up correctly
+import cdsapi
+  
+# initialize API 
+client = cdsapi.Client()
+dataset = "reanalysis-cerra-single-levels" #CERRA
+# 2024 API request #
+request_2024 = {
+    "variable": ["2m_temperature"],
+    "level_type": "surface_or_atmosphere",
+    "data_type": ["reanalysis"],
+    "year": ["2024"],
+    "month": ["05", "06", "07", "08", "09"],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30",
+        "31"
+    ],
+    "time": [
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
+    ],
+    "data_format": "netcdf",
+    #"download_format": "unarchived",
+    "area": [60.05, 24.7, 60.3, 25.28]
+}
+target_2024 = r"\\ad.helsinki.fi\home\t\terschan\Desktop\paper1\data\11.25\CERRA_hel_2024.netcdf" # target folder and name
+client.retrieve(dataset, request_2024, target_2024) # execute API request
+
+# 2024 API request #
+request_2025 = {
+    "variable": ["2m_temperature"],
+    "year": ["2025"],
+    "month": ["05", "06", "07", "08", "09"],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30",
+        "31"
+    ],
+    "time": [
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
+    ],
+    "data_format": "netcdf",
+    "download_format": "unarchived",
+    "area": [60.05, 24.7, 60.3, 25.28]
+}
+target_2025 = r"\\ad.helsinki.fi\home\t\terschan\Desktop\paper1\data\11.25\CERRA_hel_2025.netcdf" # target folder and name
+client.retrieve(dataset, request_2025, target_2025) # execute API request
+
+
+# dummy testing code provided in documentation
+#import cdsapi
+#client = cdsapi.Client()
+#dataset = 'reanalysis-era5-pressure-levels'
+#request = {
+#    'product_type': ['reanalysis'],
+#    'variable': ['geopotential'],
+#    'year': ['2024'],
+#    'month': ['03'],
+#    'day': ['01'],
+#    'time': ['13:00'],
+#    'pressure_level': ['1000'],
+#    'data_format': 'grib',
+#}
+#target = 'download.grib'
+#client.retrieve(dataset, request, target)
+
+request_2024 = {
+    "variable": ["2m_temperature"],
+    "level_type": "surface_or_atmosphere",
+    "data_type": ["reanalysis"],
+    "year": ["2024"],
+    "month": ["05", "06", "07", "08", "09"],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30",
+        "31"
+    ],
+    "time": [
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
+    ],
+    "data_format": "netcdf",
+    #"download_format": "unarchived",
+    "area": [60.05, 24.7, 60.3, 25.28]
+}
